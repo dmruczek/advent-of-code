0ee64db14cc481740f32779dcb2af7a044b81a53
Dave <dmruczek@gmail.com>
2023-12-10 18:43:32 -0500

    experimenting

--- years/2023/01/index.ts
+++ years/2023/01/index.ts
@@ -1,96 +1,139 @@
-import _, { first } from "lodash";
+import _ from "lodash";
 import * as util from "../../../util/util";
 import * as test from "../../../util/test";
 import chalk from "chalk";
 import { log, logSolution, trace } from "../../../util/log";
 import { performance } from "perf_hooks";
+import { normalizeTestCases } from "../../../util/test";
 
 const YEAR = 2023;
 const DAY = 1;
 
-// solution path: /home/trevorsg/dev/t-hugs/advent-of-code/years/2023/01/index.ts
-// data path    : /home/trevorsg/dev/t-hugs/advent-of-code/years/2023/01/data.txt
+// solution path: C:\Users\Dave\Documents\code\advent-of-code\years\2023\01\index.ts
+// data path    : C:\Users\Dave\Documents\code\advent-of-code\years\2023\01\data.txt
 // problem url  : https://adventofcode.com/2023/day/1
 
-async function p2023day1_part1(input: string, ...params: any[]) {
-	const lines = input.split("\n");
-	let sum = 0;
-	for (const line of lines) {
-		const arr = [...line];
-		const firstNumber = arr.find(x => /\d/.test(x));
-		const lastNumber = arr.findLast(x => /\d/.test(x));
-		if (firstNumber && lastNumber) {
-			//log("Adding", firstNumber, lastNumber);
-			sum += Number(firstNumber + lastNumber);
+class TrebuchetCalibrator {
+
+    processCalibrationLine(str:string ) {
+        const numbers = this.extractNumbers(str);
+        return parseInt((numbers[0] + numbers[numbers.length-1]), 10);
+    }
+
+    processCalibrationLine2(str:string ) {
+        const numbers = this.extractNumbers2(str);
+        return (numbers[0]*10) + numbers[numbers.length-1];
+    }
+
+    extractNumbers(str:string) : RegExpMatchArray {
+		const result = str.match(/\d/g);
+		if (result === null) {
+			throw 'error';
 		}
-	}
-	return sum;
+		return result;
+    }
+
+    extractNumbers2(str:string) {
+
+        let numArray = new Array(str.length);
+        const numbersToSearch = [
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"
+        ];
+
+        for (let numToSearch of numbersToSearch) {
+            let startIndex = 0, index;
+            while ((index = str.indexOf(numToSearch, startIndex)) > -1) {
+                numArray[index] = this.convertToNumber(numToSearch);
+                startIndex = index + numToSearch.length;
+            }
+        }
+
+        numArray = numArray.filter(n => n);
+
+        return numArray;
+
+    }
+
+    convertToNumber(str:string) :number {
+
+        const strMap: Record<string, number> = {
+            "one": 1,
+            "two": 2,
+            "three": 3,
+            "four": 4,
+            "five": 5,
+            "six": 6,
+            "seven": 7,
+            "eight": 8,
+            "nine": 9,
+        };
+        if (strMap[str]) {
+            return strMap[str];
+        }
+        return parseInt(str, 10);
+    }
+
+    calibrate(data: string) {
+        const stringArray = data.split('\n');
+        let total = 0;
+        for (let str of stringArray) {
+            total += this.processCalibrationLine(str);
+        }
+        return total;
+    }
+
+    calibrate2(data: string) {
+        const stringArray = data.split('\n');
+        let total = 0;
+        for (let str of stringArray) {
+            total += this.processCalibrationLine2(str);
+        }
+        return total;
+    }
+}
+
+async function p2023day1_part1(input: string, ...params: any[]) {
+	const trebuchetCalibrator = new TrebuchetCalibrator();
+	return trebuchetCalibrator.calibrate(input);
 }
 
 async function p2023day1_part2(input: string, ...params: any[]) {
-	const lines = input.split("\n");
-	let sum = 0;
-	const fre = /(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|(zero)/;
-	const bre = /(\d)|(eno)|(owt)|(eerht)|(ruof)|(evif)|(xis)|(neves)|(thgie)|(enin)|(orez)/;
-	const english: { [key: string]: number } = {
-		one: 1,
-		two: 2,
-		three: 3,
-		four: 4,
-		five: 5,
-		six: 6,
-		seven: 7,
-		eight: 8,
-		nine: 9,
-		zero: 0,
-	};
-	for (const line of lines) {
-		const first = fre.exec(line)?.[0];
-		const lastReversed = bre.exec([...line].reverse().join(""))?.[0];
-		if (first && lastReversed) {
-			const last = [...lastReversed].reverse().join("");
-			const firstVal = first.length > 1 ? english[first] : Number(first);
-			const lastVal = last.length > 1 ? english[last] : Number(last);
-			sum += Number(String(firstVal) + String(lastVal));
-		}
-	}
-	return sum;
+	const trebuchetCalibrator = new TrebuchetCalibrator();
+	return trebuchetCalibrator.calibrate2(input);
 }
 
 async function run() {
-	const part1tests: TestCase[] = [
-		{
-			input: `1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet`,
-			extraArgs: [],
-			expected: `142`,
-		},
-	];
-	const part2tests: TestCase[] = [
-		{
-			input: `two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen`,
-			extraArgs: [],
-			expected: `281`,
-		},
-	];
+	const part1tests: TestCase[] = [];
+	const part2tests: TestCase[] = [];
+
+	const [p1testsNormalized, p2testsNormalized] = normalizeTestCases(part1tests, part2tests);
 
 	// Run tests
 	test.beginTests();
 	await test.section(async () => {
-		for (const testCase of part1tests) {
+		for (const testCase of p1testsNormalized) {
 			test.logTestResult(testCase, String(await p2023day1_part1(testCase.input, ...(testCase.extraArgs || []))));
 		}
 	});
 	await test.section(async () => {
-		for (const testCase of part2tests) {
+		for (const testCase of p2testsNormalized) {
 			test.logTestResult(testCase, String(await p2023day1_part2(testCase.input, ...(testCase.extraArgs || []))));
 		}
 	});
@@ -103,7 +146,7 @@ zoneight234
 	const part1Solution = String(await p2023day1_part1(input));
 	const part1After = performance.now();
 
-	const part2Before = performance.now();
+	const part2Before = performance.now()
 	const part2Solution = String(await p2023day1_part2(input));
 	const part2After = performance.now();
 
--- years/2023/22/index.ts
+++ years/2023/22/index.ts
@@ -9,8 +9,8 @@ import { normalizeTestCases } from "../../../util/test";
 const YEAR = 2023;
 const DAY = 22;
 
-// solution path: /home/trevorsg/dev/t-hugs/advent-of-code/years/2023/22/index.ts
-// data path    : /home/trevorsg/dev/t-hugs/advent-of-code/years/2023/22/data.txt
+// solution path: C:\Users\Dave\Documents\code\advent-of-code\years\2023\22\index.ts
+// data path    : C:\Users\Dave\Documents\code\advent-of-code\years\2023\22\data.txt
 // problem url  : https://adventofcode.com/2023/day/22
 
 async function p2023day22_part1(input: string, ...params: any[]) {
